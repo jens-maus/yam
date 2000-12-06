be954e3918ff368ab193da136a3f84613dc4ef82
Jacob Laursen <jacob@vindvejr.dk>
2000-12-06 14:31:51 +0000

    Added md5.h and global.h to be able to compile

---
+++ yamos/global.h
@@ -0,0 +1,10 @@
+/* global.h - RSAREF types and constants */
+
+/* POINTER defines a generic pointer type */
+typedef unsigned char *POINTER;
+
+/* UINT2 defines a two byte word */
+typedef unsigned short int UINT2;
+
+/* UINT4 defines a four byte word */
+typedef unsigned long int UINT4;
