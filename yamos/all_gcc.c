563b37aa2ecfbb81adb6f26d0f98c27a9e2305d0
Jörg Strohmayer <j_s@gmx.de>
2000-12-15 17:28:13 +0000

    gcc functions

---
+++ yamos/all_gcc.c
@@ -0,0 +1,14 @@
+#include "extrasrc/md5.c"
+#include "extrasrc/astcsma.c"
+#include "extrasrc/getft.c"
+#include "extrasrc/mkdir.c"
+#include "extrasrc/stccpy.c"
+#include "extrasrc/stcgfe.c"
+#include "extrasrc/stcgfn.c"
+#include "extrasrc/stpblk.c"
+#include "extrasrc/strmfp.c"
+#include "extrasrc/strsfn.c"
+#include "extrasrc/wbpath.c"
+#include "extrasrc/NewReadArgs.c"
+
+int __oslibversion = 0;
