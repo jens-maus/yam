d5c2e5f998c5af7f9db09ba18c5d79e0b078c8e0
Jörg Strohmayer <j_s@gmx.de>
2001-03-09 19:52:17 +0000

    KPrintf replacement.

--- yamos/all_dice.c
+++ yamos/all_dice.c
@@ -11,3 +11,20 @@
 #include "extrasrc/NewReadArgs.c"
 #include "extrasrc/stch_i.c"
 #include "extrasrc/dice.c"
+
+void wbmain(struct WBStartup *wbs)
+{
+   main(0, (char**)wbs);
+}
+
+#include <stdarg.h>
+int KPrintf(const char *format)
+{
+   int n; va_list vl;
+
+   va_start(vl,format);
+   n=fprintf(stderr,format,vl);
+   va_end(vl);
+   fflush(stderr);
+   return n;
+}
