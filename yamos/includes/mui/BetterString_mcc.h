7d6873b936e47975632ec97b479f910ab473c2f9
Allan Odgaard <git@abetarda.com>
2001-06-17 08:46:47 +0000

    * Updated BetterString_mcc.h (latest Betterstring.mcc can be found in the       file area of YahooGroups.Com/yamos/).

--- yamos/includes/mui/BetterString_mcc.h
+++ yamos/includes/mui/BetterString_mcc.h
@@ -1,6 +1,6 @@
 /*
 **
-** $VER: BetterString_mcc.h V11.5 (21-May-00)
+** $VER: BetterString_mcc.h V11.6 (19-Dec-00)
 ** Copyright © 2000 Allan Odgaard. All rights reserved.
 **
 */
@@ -19,6 +19,8 @@
 #define MUIA_BetterString_NoInput       0xad001007
 #define MUIA_BetterString_SelectSize    0xad001001
 #define MUIA_BetterString_StayActive    0xad001003
+#define MUIA_BetterString_KeyUpFocus    0xad001008
+#define MUIA_BetterString_KeyDownFocus  0xad001009
 #define MUIM_BetterString_ClearSelected 0xad001004
 #define MUIM_BetterString_FileNameStart 0xad001006
 #define MUIM_BetterString_Insert        0xad001002
@@ -26,6 +28,8 @@
 #define MUIV_BetterString_Insert_StartOfString  0x00000000
 #define MUIV_BetterString_Insert_EndOfString    0xfffffffe
 #define MUIV_BetterString_Insert_BufferPos      0xffffffff
+
+#define MUIV_BetterString_BufferPos_End         0xffffffff
 
 #define MUIR_BetterString_FileNameStart_Volume  0xffffffff
 
