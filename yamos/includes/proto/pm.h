c4278070dcf0e86e7e09bb5db187e320ecde8df5
Giles Burdett <layabouts@the-giant-sofa.demon.co.uk>
2001-06-18 23:51:10 +0000

    now includes support for more compilers

--- yamos/includes/proto/pm.h
+++ yamos/includes/proto/pm.h
@@ -1,7 +1,28 @@
-#ifndef POPUPMENU_PROTO_H
-#define POPUPMENU_PROTO_H
+#ifndef PROTO_POPUPMENU_H
+#define PROTO_POPUPMENU_H
+
+#ifndef EXEC_TYPES_H
 #include <exec/types.h>
+#endif
+#ifndef CLIB_POPUPMENU_PROTOS_H
 #include <clib/pm_protos.h>
-extern struct PopupMenuBase	*PopupMenuBase;
+#endif
+
+#ifndef __NOLIBBASE__
+extern struct PopupMenuBase *
+  #ifdef __CONSTLIBBASEDECL__
+  __CONSTLIBBASEDECL__
+  #endif /* __CONSTLIBBASEDECL__ */
+PopupMenuBase;
+#endif
+
+#if defined(__GNUC__) || defined(__VBCC__)
+#include <inline/pm.h>
+#endif
+
+#if defined(LATTICE) || defined(__SASC) || defined(_DCC) || defined(__STORM__)
 #include <pragmas/pm_pragmas.h>
 #endif
+
+#endif  /*  PROTO_POPUPMENU_H  */
+
