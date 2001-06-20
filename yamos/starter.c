8f842b0ad80eb2ba1cb588bf29a2f7435dbf3e27
Gunther Nikl <gnikl@baltic.net>
2001-06-20 14:50:48 +0000

    cleanup

--- yamos/starter.c
+++ yamos/starter.c
@@ -12,7 +12,7 @@
   To make two version of the program 'PROG', compile this program with
   a 68k gcc with:
 
-     gcc -nostdlib -o PROG -O2 -noixemul -DNAME="\"PROG\"" starter.c
+   gcc -nostdlib -DNAME=\"PROG\" -O -fomit-frame-pointer starter.c -o PROG
 
   Then name the 68k version of 'PROG' 'PROG.amigaos', the MorphOS version
   'PROG.morphos'.
@@ -21,101 +21,90 @@
 
 *************************************************************************/
 
-#define __NOLIBBASE
-#include <exec/exec.h>
-#include <dos/dos.h>
 #include <dos/dosextens.h>
+#include <proto/intuition.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
-#include <proto/intuition.h>
 
 #define REG(r, x) x __asm(#r)
 
 asm("
 	.text
-	bra _start
-");
+	jra _start
 
-int call(REG(a0, char *line), REG(d0, int len), REG(a1, void *start));
-asm("
-	.text
-_call:
-	moveml	d2-d7/a2-a6,sp@-
-	jbsr	 (a1)
+_call:	moveml	d2-d7/a2-a6,sp@-
+	addal	a1,a1
+	addal	a1,a1
+	jsr	a1@(4)
 	moveml	sp@+,d2-d7/a2-a6
 	rts
 ");
 
+int call(REG(a0, char *line), REG(d0, int len), REG(a1, BPTR start));
 
-int start(REG(a0, char *line), REG(d0, int len)) {
-    struct Library *SysBase = *(struct Library **)4;
-    struct Library *DOSBase;
-    struct Library *IntuitionBase;
-    struct Process *proc = (struct Process *)FindTask(NULL);
-    struct Message *wbmsg;
-    int ret = RETURN_FAIL;
-    BPTR seg = 0;
+int start(REG(a0, char *line), REG(d0, int len))
+{ APTR IntuitionBase, DOSBase, SysBase = *(APTR *)4;
+  struct Message *wbmsg;
+  struct Process *proc;
+  int ret = RETURN_FAIL;
+  BPTR seg;
 
-    if (proc->pr_CLI)
-	wbmsg = NULL;
+  proc = (struct Process *)FindTask(NULL);
+
+  if (!proc->pr_CLI) {
+    WaitPort(&proc->pr_MsgPort);
+    wbmsg = GetMsg(&proc->pr_MsgPort);
+  }
+  else {
+    wbmsg = NULL;
+  }
+
+  if ((DOSBase = OpenLibrary("dos.library", 39))) {
+
+    if (!FindResident("MorphOS") || (0 == (seg = LoadSeg("PROGDIR:" NAME ".morphos"))))
+      seg = LoadSeg("PROGDIR:" NAME ".amigaos");
+
+    if (seg) {
+
+      if (wbmsg) {
+        PutMsg(&proc->pr_MsgPort, wbmsg);
+        wbmsg = NULL;
+      }
+
+      ret = call(line, len, seg);
+
+      UnLoadSeg(seg);
+    }
+    else if (wbmsg) {
+
+      if ((IntuitionBase = OpenLibrary("intuition.library", 39))) {
+
+        static const struct EasyStruct params = {
+          sizeof(struct EasyStruct),
+          0,
+          "Error",
+          "Can't load \"PROGDIR:" NAME ".amigaos\".",
+          "Ok"
+        };
+
+        EasyRequestArgs(NULL, &params, NULL, NULL);
+
+        CloseLibrary(IntuitionBase);
+      }
+    }
     else {
-	WaitPort(&proc->pr_MsgPort);
-	wbmsg = GetMsg(&proc->pr_MsgPort);
+
+      PutStr("Can't load \"PROGDIR:" NAME ".amigaos\".\n");
+
     }
 
-    if (DOSBase = OpenLibrary("dos.library", 39)) {
+    CloseLibrary(DOSBase);
+  }
 
-	if (FindResident("MorphOS")) {
-	    seg = LoadSeg("PROGDIR:" NAME ".morphos");
-	}
+  if (wbmsg) {
+    Forbid();
+    ReplyMsg(wbmsg);
+  }
 
-	if (!seg)
-	    seg = LoadSeg("PROGDIR:" NAME ".amigaos");
-
-	if (seg) {
-	    int (*start)(REG(a0, char *line), REG(d0, int len));
-
-	    start = (void *)((BPTR *)BADDR(seg) + 1);
-
-	    if (wbmsg) {
-		PutMsg(&proc->pr_MsgPort, wbmsg);
-		wbmsg = NULL;
-	    }
-
-	    ret = call(line, len, start);
-
-	    UnLoadSeg(seg);
-	}
-
-	if (!seg) {
-	    if (wbmsg) {
-		
-		if (IntuitionBase = (APTR)OpenLibrary("intuition.library", 39)) {
-		    static struct EasyStruct params = {
-			sizeof(struct EasyStruct),
-			0,
-			"Error",
-			"Can't load \"PROGDIR:" NAME ".amigaos\".",
-			"Ok"
-		    };
-
-		    EasyRequestArgs(NULL, &params, NULL, NULL);
-
-		    CloseLibrary(IntuitionBase);
-		}
-
-	    } else
-		PutStr("Can't load \"PROGDIR:" NAME ".amigaos\".\n");
-	}
-
-	CloseLibrary(DOSBase);
-    }
-
-    if (wbmsg) {
-	Forbid();
-	ReplyMsg(wbmsg);
-    }
-
-    return ret;
+  return ret;
 }
-
