9e40955a29cfa340c01b39a2ad3b4ed648683b67
Dirk Stöcker <webmaster@dstoecker.de>
2001-06-24 10:14:00 +0000

    moved searchwindow class

--- yamos/classes/searchwindow.c
+++ yamos/classes/searchwindow.c
@@ -1,8 +1,35 @@
-// Superclass: MUIC_Window
-// Description: Window where user inputs search string and options.
-// $Id$
+/***************************************************************************
+
+ YAM - Yet Another Mailer
+ Copyright (C) 1995-2000 by Marcel Beck <mbeck@yam.ch>
+ Copyright (C) 2000-2001 by YAM Open Source Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ YAM Official Support Site :  http://www.yam.ch
+ YAM OpenSource project    :  http://sourceforge.net/projects/yamos/
+
+ $Id$
+
+ Superclass:  MUIC_Window
+ Description: Window where user inputs search string and options.
+
+***************************************************************************/
 
 #include <clib/alib_protos.h>
+#include <libraries/iffparse.h>
 #include <libraries/mui.h>
 #include <mui/BetterString_mcc.h>
 #include <mui/TextEditor_mcc.h>
@@ -20,7 +47,6 @@
 #define ATTR(attr) case MUIA_Searchwindow_## attr
 /* ---------------------------------- */
 
-#define MAKE_ID(a,b,c,d) ((ULONG) (a)<<24 | (ULONG) (b)<<16 | (ULONG) (c)<<8 | (ULONG) (d))
 #define inittags(msg) (((struct opSet *)msg)->ops_AttrList)
 #define GETDATA struct Data *data = (struct Data *)INST_DATA(cl,obj)
 
