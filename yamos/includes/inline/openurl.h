37138c16ecfd4dd753ae78a3061b1133fddb8982
Gunther Nikl <gnikl@baltic.net>
2001-07-31 14:19:22 +0000

    use "newstyle" inlines

--- yamos/includes/inline/openurl.h
+++ yamos/includes/inline/openurl.h
@@ -1,114 +1,43 @@
+/* Automatically generated header! Do not edit! */
+
 #ifndef _INLINE_OPENURL_H
 #define _INLINE_OPENURL_H
 
-#ifndef __INLINE_STUB_H
-#include <inline/stubs.h>
-#endif
+#ifndef __INLINE_MACROS_H
+#include <inline/macros.h>
+#endif /* !__INLINE_MACROS_H */
 
-#ifndef BASE_EXT_DECL
-#define BASE_EXT_DECL
-#define BASE_EXT_DECL0 extern struct Library *OpenURLBase;
-#endif
-#ifndef BASE_PAR_DECL
-#define BASE_PAR_DECL
-#define BASE_PAR_DECL0 void
-#endif
-#ifndef BASE_NAME
-#define BASE_NAME OpenURLBase
-#endif
+#ifndef OPENURL_BASE_NAME
+#define OPENURL_BASE_NAME OpenURLBase
+#endif /* !OPENURL_BASE_NAME */
 
-BASE_EXT_DECL0
+#define URL_FreePrefs(up) \
+	LP1NR(0x2a, URL_FreePrefs, struct URL_Prefs *, up, a0, \
+	, OPENURL_BASE_NAME)
 
-extern __inline BOOL
-URL_OpenA(BASE_PAR_DECL STRPTR url, struct TagItem * tags)
-{
-  BASE_EXT_DECL
-  register BOOL res __asm("d0");
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  register STRPTR a0 __asm("a0") = url;
-  register struct TagItem * a1 __asm("a1") = tags;
-  __asm volatile ("jsr a6@(-0x1E:W)"
-  : "=r" (res)
-  : "r" (a6), "r" (a0), "r" (a1)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-  return res;
-}
+#define URL_GetDefaultPrefs() \
+	LP0(0x36, struct URL_Prefs *, URL_GetDefaultPrefs, \
+	, OPENURL_BASE_NAME)
 
-extern __inline BOOL URL_Open(STRPTR url, Tag tags, ...)
-{
-  return URL_OpenA(url, (struct TagItem *) &tags);
-}
+#define URL_GetPrefs() \
+	LP0(0x24, struct URL_Prefs *, URL_GetPrefs, \
+	, OPENURL_BASE_NAME)
 
-extern __inline struct URL_Prefs *
-URL_GetPrefs(BASE_PAR_DECL0)
-{
-  BASE_EXT_DECL
-  register struct URL_Prefs * res __asm("d0");
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  __asm volatile ("jsr a6@(-0x24:W)"
-  : "=r" (res)
-  : "r" (a6)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-  return res;
-}
+#define URL_LaunchPrefsApp() \
+	LP0(0x3c, BOOL, URL_LaunchPrefsApp, \
+	, OPENURL_BASE_NAME)
 
-extern __inline VOID
-URL_FreePrefs(BASE_PAR_DECL struct URL_Prefs * up)
-{
-  BASE_EXT_DECL
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  register struct URL_Prefs * a0 __asm("a0") = up;
-  __asm volatile ("jsr a6@(-0x2A:W)"
-  : /* No Output */
-  : "r" (a6), "r" (a0)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-}
+#define URL_OpenA(url, tags) \
+	LP2(0x1e, BOOL, URL_OpenA, STRPTR, url, a0, struct TagItem *, tags, a1, \
+	, OPENURL_BASE_NAME)
 
-extern __inline BOOL
-URL_SetPrefs(BASE_PAR_DECL struct URL_Prefs * up, BOOL permanent)
-{
-  BASE_EXT_DECL
-  register BOOL res __asm("d0");
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  register struct URL_Prefs * a0 __asm("a0") = up;
-  register BOOL d0 __asm("d0") = permanent;
-  __asm volatile ("jsr a6@(-0x30:W)"
-  : "=r" (res)
-  : "r" (a6), "r" (a0), "r" (d0)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-  return res;
-}
+#ifndef NO_INLINE_STDARG
+#define URL_Open(a0, tags...) \
+	({ULONG _tags[] = { tags }; URL_OpenA((a0), (struct TagItem *)_tags);})
+#endif /* !NO_INLINE_STDARG */
 
-extern __inline struct URL_Prefs *
-URL_GetDefaultPrefs(BASE_PAR_DECL0)
-{
-  BASE_EXT_DECL
-  register struct URL_Prefs * res __asm("d0");
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  __asm volatile ("jsr a6@(-0x36:W)"
-  : "=r" (res)
-  : "r" (a6)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-  return res;
-}
+#define URL_SetPrefs(up, permanent) \
+	LP2(0x30, BOOL, URL_SetPrefs, struct URL_Prefs *, up, a0, BOOL, permanent, d0, \
+	, OPENURL_BASE_NAME)
 
-extern __inline BOOL
-URL_LaunchPrefsApp(BASE_PAR_DECL0)
-{
-  BASE_EXT_DECL
-  register BOOL res __asm("d0");
-  register struct Library *a6 __asm("a6") = BASE_NAME;
-  __asm volatile ("jsr a6@(-0x3C:W)"
-  : "=r" (res)
-  : "r" (a6)
-  : "d0", "d1", "a0", "a1", "fp0", "fp1", "cc", "memory");
-  return res;
-}
-
-#undef BASE_EXT_DECL
-#undef BASE_EXT_DECL0
-#undef BASE_PAR_DECL
-#undef BASE_PAR_DECL0
-#undef BASE_NAME
-
-#endif /*  _INLINE_OPENURL_H  */
+#endif /* !_INLINE_OPENURL_H */
