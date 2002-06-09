72c8c546daa3ecdd2d5a6546772abfb5d207ecdb
Jens Maus <mail@jens-maus.de>
2002-06-09 15:05:28 +0000

    - added __MORPHOS__ and __libnix define checks as the libnix library   of MorphOS contains many functions we previously implemented our own. - now this file is also responsible for the MorphOS tree.

--- yamos/all_gcc.c
+++ yamos/all_gcc.c
@@ -2,7 +2,7 @@
 
  YAM - Yet Another Mailer
  Copyright (C) 1995-2000 by Marcel Beck <mbeck@yam.ch>
- Copyright (C) 2000-2001 by YAM Open Source Team
+ Copyright (C) 2000-2002 by YAM Open Source Team
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -26,12 +26,17 @@
 ***************************************************************************/
 
 #include "extrasrc/astcsma.c"
-#include "extrasrc/getft.c"
-#include "extrasrc/stccpy.c"
-#include "extrasrc/stcgfe.c"
-#include "extrasrc/stcgfn.c"
-#include "extrasrc/stpblk.c"
-#include "extrasrc/strmfp.c"
-#include "extrasrc/strsfn.c"
 #include "extrasrc/NewReadArgs.c"
 #include "extrasrc/dice.c"
+
+// If we don`t have MorphOS libnix lets
+// use our own implementations.
+#if !defined(__MORPHOS__) || !defined(__libnix)
+  #include "extrasrc/getft.c"
+  #include "extrasrc/stccpy.c"
+  #include "extrasrc/stcgfe.c"
+  #include "extrasrc/stcgfn.c"
+  #include "extrasrc/stpblk.c"
+  #include "extrasrc/strmfp.c"
+  #include "extrasrc/strsfn.c"
+#endif
