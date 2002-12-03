49bbc3a5f9a832505c191440358fc29b9663366c
Gunther Nikl <gnikl@baltic.net>
2002-12-03 16:33:18 +0000

    FreeStrBuf: use sizeof() instead of hardcoded constant

--- yamos/YAM_utilities.h
+++ yamos/YAM_utilities.h
@@ -210,7 +210,7 @@ struct NewToolbarEntry
 #define SetHelp(o,str)        set(o, MUIA_ShortHelp, GetStr(str))
 #define DisposeModulePush(m)  DoMethod(G->App, MUIM_Application_PushMethod, G->App, 3, MUIM_CallHook, &DisposeModuleHook, m)
 #define MyStrCpy(a,b)         { strncpy((a),(b), sizeof(a)); (a)[sizeof(a)-1] = 0; }
-#define FreeStrBuf(str)       ((str) ? free(((char *)(str))-4) : (void)0)
+#define FreeStrBuf(str)       ((str) ? free(((char *)(str))-sizeof(size_t)) : 0)
 
 extern int            BusyLevel;
 extern struct Hook    GeneralDesHook;
