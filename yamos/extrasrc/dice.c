c431aad886092106842e952c9064eb579553128d
Gunther Nikl <gnikl@baltic.net>
2003-02-24 10:38:19 +0000

    removed popupmenu stubs

--- yamos/extrasrc/dice.c
+++ yamos/extrasrc/dice.c
@@ -12,14 +12,6 @@ LONG XpkUnpackTags(ULONG tag, ...)
 BOOL URL_Open(STRPTR str, ULONG tag, ...)
 { return URL_OpenA(str,(struct TagItem *)&tag); }
 
-#include <proto/pm.h>
-struct PopupMenu *PM_MakeItem(ULONG tag, ...)
-{ return PM_MakeItemA((struct TagItem *)&tag); }
-struct PopupMenu *PM_MakeMenu(ULONG tag, ...)
-{ return PM_MakeMenuA((struct TagItem *)&tag); }
-ULONG PM_OpenPopupMenu(struct Window *prevwnd, ULONG tag, ...)
-{ return PM_OpenPopupMenuA(prevwnd,(struct TagItem *)&tag); }
-
 #include <proto/amissl.h>
 long InitAmiSSL(ULONG tag, ...)
 { return InitAmiSSLA((struct TagItem *)&tag); }
