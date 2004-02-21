3241e39210fba443a7b4d61c0b8a0b3c9ca39dbf
Jens Maus <mail@jens-maus.de>
2004-02-21 22:10:26 +0000

    - you may believe it or not, but proto/intuition.h is required on OS4 because   we need the inline4 macros which are only included by the proto file.

--- yamos/YAM_ER.c
+++ yamos/YAM_ER.c
@@ -2,7 +2,7 @@
 
  YAM - Yet Another Mailer
  Copyright (C) 1995-2000 by Marcel Beck <mbeck@yam.ch>
- Copyright (C) 2000-2001 by YAM Open Source Team
+ Copyright (C) 2000-2004 by YAM Open Source Team
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@
 
 #include <clib/alib_protos.h>
 #include <libraries/iffparse.h>
+#include <proto/intuition.h>
 #include <proto/muimaster.h>
 
 #include "SDI_hook.h"
