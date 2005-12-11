aa1279c3467dccab97eb7b7ef4fc044e0dda0df8
Jens Maus <mail@jens-maus.de>
2005-12-11 23:43:07 +0000

    - locale/YAM.cd, locale/cleanup.sh: added a small shell script   for searching for orphaned catalog strings in the YAM.cd file. It will   automatically strip these orphaned strings off the YAM.cd file causing   the resulting binary of YAM to be significantely smaller afterwards.   Please note however, that the script still needs to be adapted to also   search through the *.ct files, stripping off not required strings.

---
+++ yamos/locale/cleanup.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#
+# This tiny script takes the YAM.cd file and compares its defined locale string
+# with the actual source code of YAM. Found orphaned locale strings will then be
+# automatically removed from the .cd file and a cleaned up "YAM.cd.new" file will be
+# written back to the harddisk.
+#
+# $Id$
+#
+
+CDFILE="./YAM.cd"
+SOURCES="../*.c ../classes/*.c"
+TMPCD="/tmp/YAM.cd"
+
+########################################################
+# Script starts here
+#
+echo "Scanning $CDFILE for orphaned catalog IDs:"
+
+# get all currently defined MSG_ tægs in the .cd file
+MSGTAGS=`awk '/^MSG_/ { print $1 }' $CDFILE | xargs` 
+
+# now we pick each MSG tag and search in the sources dirs for GetStr() function
+# calls
+ORPHANEDTAGS=""
+for tag in $MSGTAGS; do
+  grep "$tag" $SOURCES | grep -v "_$tag" >/dev/null
+  if [ $? == 1 ]; then
+    echo "$tag not found in source code"
+    ORPHANEDTAGS="$ORPHANEDTAGS $tag "
+  fi
+done
+
+# now we process every orphaned tag and remove it from the YAM.cd file
+cp $CDFILE $TMPCD.in
+for otag in $ORPHANEDTAGS; do
+  awk "BEGIN { tagfound=0; }        \
+       {                            \
+         if(tagfound == 1)          \
+         {                          \
+           if(\$1 ~ /^MSG_/)        \
+           {                        \
+             tagfound=0;            \
+           }                        \
+         }                          \
+                                    \
+         if(\$1 ~ /^$otag/)         \
+         {                          \
+           tagfound=1;              \
+         }                          \
+                                    \
+         if(tagfound == 0)          \
+         {                          \
+           print \$0;               \
+         }                          \
+       }" $TMPCD.in > $TMPCD.out
+
+  cp $TMPCD.out $TMPCD.in
+done
+
+# copy the processed output file to YAM.cd.new
+cp $TMPCD.out $CDFILE.new 2>/dev/null
+rm $TMPCD.out $TMPCD.in 2>/dev/null
