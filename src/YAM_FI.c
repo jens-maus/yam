940788367742b8feb3c852e92d958b68f1271811
Jens Maus <mail@jens-maus.de>
2006-03-29 21:47:37 +0000

    - fixed on still existant tv_sec/tv_usec occurance to tv_secs/tv_micro

--- src/YAM_FI.c
+++ src/YAM_FI.c
@@ -690,7 +690,7 @@ HOOKPROTONHNONP(FI_SearchFunc, void)
             SubTime(&delta, &last);
 
             // update the display at least twice a second
-            if(delta.tv_sec > 0 || delta.tv_usec > 250000)
+            if(delta.tv_secs > 0 || delta.tv_micro > 250000)
             {
               set(ga, MUIA_Gauge_Current, progress);
 
