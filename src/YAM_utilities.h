146f71bffb214bc7c34c2dce1b4b01ee6dd50ad1
Jens Maus <mail@jens-maus.de>
2006-04-18 07:16:17 +0000

    * YAM_utilities.h: revised our own "struct TimeVal" fallback structure   definitions to also be defined on older OS4 SDK's.

--- src/YAM_utilities.h
+++ src/YAM_utilities.h
@@ -92,8 +92,9 @@ struct NewToolbarEntry
 // since the Amiga's timeval structure was renamed to
 // "struct TimeVal" in OS4 (to prevent clashes with the POSIX one)
 // we require to define that slightly compatible structure on our
-// own in case we compile YAM for something else than OS4
-#if !defined(__amigaos4__)
+// own in case we compile YAM for something else than OS4 or in case
+// an older SDK is used.
+#if !defined(__amigaos4__) || !defined(__NEW_TIMEVAL_DEFINITION_USED__)
 struct TimeVal
 {
   ULONG Seconds;
