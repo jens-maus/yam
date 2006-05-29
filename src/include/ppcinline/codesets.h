30d982f16014bb9532e3790c8fdb82f700025192
Jens Maus <mail@jens-maus.de>
2006-05-29 21:57:54 +0000

    - updated includes of codesets.library according to latest 6.2 release

--- src/include/ppcinline/codesets.h
+++ src/include/ppcinline/codesets.h
@@ -1,3 +1,25 @@
+/***************************************************************************
+
+ codesets.library - Amiga shared library for handling different codesets
+ Copyright (C) 2001-2005 by Alfonso [alfie] Ranieri <alforan@tin.it>.
+ Copyright (C) 2005-2006 by codesets.library Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ codesets.library project: http://sourceforge.net/projects/codesetslib/
+
+ $Id$
+
+***************************************************************************/
+
 /* Automatically generated header! Do not edit! */
 
 #ifndef _PPCINLINE_CODESETS_H
