6480b11ceaa5990ec6a23b4d73062c62a5c4ca88
Jens Maus <mail@jens-maus.de>
2006-08-21 00:15:38 +0000

    * HTML2Mail.c: implemented a first version of a simple, flex-based html to   plaintext converter which accepts html infected text and automatically   converts it to RFC822 conform mail text.   Please note that this html converter is meant to kept simple with only a   low amount of effort in properly converting all kind of HTML magic.   Especially, things like HTML tables/frames/css etc. will never be supported   by it. For displaying such complex HTML stuff a later version will contain   the functionality of viewing such HTML via htmlview.mcc. So this converter   is really only meant for simple stuff. So please, don't fire up your feature   requests too fast :)

---
+++ src/HTML2Mail.h
@@ -0,0 +1,47 @@
+#ifndef HTML2MAIL_H
+#define HTML2MAIL_H
+
+/***************************************************************************
+
+ YAM - Yet Another Mailer
+ Copyright (C) 1995-2000 by Marcel Beck <mbeck@yam.ch>
+ Copyright (C) 2000-2006 by YAM Open Source Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ YAM Official Support Site :  http://www.yam.ch
+ YAM OpenSource project    :  http://sourceforge.net/projects/yamos/
+
+ $Id: YAM_mail_lex.h 2046 2006-03-13 12:13:58Z damato $
+
+***************************************************************************/
+
+enum htmlTagType
+{
+  ht_PARAGRAPH = 1,
+  ht_BR,
+  ht_HR,
+  ht_UNKNOWN,
+  ht_NBSP,
+  ht_AMP,
+  ht_GT,
+  ht_LT,
+  ht_QUOT,
+  ht_NORMALTEXT
+};
+
+char *html2mail(char *htmlTxt);
+
+#endif /* HTML2MAIL_H */
