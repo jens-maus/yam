d7411e40aeb841c4048ef76176dc13fef74f52f2
Jens Maus <mail@jens-maus.de>
2007-07-03 09:19:04 +0000

    * extrasrc/vasprintf.c: added the missing VA_COPY() calls which were   hopefully the reason why this function wasn't working for our MOS builds.

--- src/extrasrc/vasprintf.c
+++ src/extrasrc/vasprintf.c
@@ -2,16 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if defined(__MORPHOS__)
+#define HAVE_VA_COPY
+#endif
+
+#ifndef VA_COPY
+#ifdef HAVE_VA_COPY
+#define VA_COPY(dest, src) __va_copy(dest, src)
+#else
+#define VA_COPY(dest, src) (dest) = (src)
+#endif
+#endif
+
 int vasprintf(char **ptr, const char * format, va_list ap)
 {
   int ret;
+  va_list ap2;
 
-  ret = vsnprintf(NULL, 0, format, ap);
+  VA_COPY(ap2, ap);
+  ret = vsnprintf(NULL, 0, format, ap2);
   if(ret <= 0) return ret;
 
   (*ptr) = (char *)malloc(ret+1);
   if(!*ptr) return -1;
-  ret = vsnprintf(*ptr, ret+1, format, ap);
+  VA_COPY(ap2, ap);
+  ret = vsnprintf(*ptr, ret+1, format, ap2);
 
   return ret;
 }
