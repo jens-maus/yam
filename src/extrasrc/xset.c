6f7e919808917e31a053554d34e961cd279484b1
Thore Böckelmann <tboeckel@gmx.de>
2007-11-07 09:42:22 +0000

    * extrasrc/xset.c: now a new tag list is built from the supplied tag list   to be able to terminate this one by TAG_DONE. This requires some guess-   work and is limited to 5 attributes to be set at most with one call of   xset(). That's all what can be done to keep up SAS/C compilability if   nobody else know how to handle this case.

--- src/extrasrc/xset.c
+++ src/extrasrc/xset.c
@@ -1,9 +1,36 @@
 #include <proto/intuition.h>
+#include <proto/utility.h>
+
+#warning possible incomplete tag list parsing!!
 
 ULONG xset(Object *obj, ...)
 {
-  // Note: in contrast to the xset() macro this function does NOT
-  // ensure that the supplied tag list is terminated by TAG_DONE!!
-  // This function only exists to keep SAS/C compatibility!
-  return SetAttrsA(obj, (struct TagItem *)(&obj + 1));
+  struct TagItem *tags = (struct TagItem *)(&obj + 1);
+  struct TagItem *tag;
+  struct TagItem newTags[6];
+  struct TagItem *newTag = newTags;
+  int i;
+
+  // We will try to rebuild a new tag list with a terminating TAG_DONE.
+  // This approach is not perfect as we have to "guess" the end of the
+  // supplied tag list or at least have to set a definite limit on the
+  // number of usable attributes. xset() calls usually are quite "simple",
+  // hence we accept up to 5 attributes at most to be set with one call.
+  i = 0;
+  while(i < (sizeof(newTags) / sizeof(newTags[0])) - 1 && (tag = NextTagItem(&tags)) != NULL)
+  {
+    if(tag->ti_Tag >= TAG_USER)
+    {
+      // copy any user defined tag item
+      newTag->ti_Tag = tag->ti_Tag;
+      newTag->ti_Data = tag->ti_Data;
+      newTag++;
+      i++;
+    }
+  }
+
+  // add the terminating TAG_DONE
+  newTag->ti_Tag = TAG_DONE;
+
+  return SetAttrsA(obj, newTags);
 }
