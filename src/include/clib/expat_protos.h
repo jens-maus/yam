afe63bb955825ba184946574b45d186adc73ca96
Thore Böckelmann <tboeckel@gmx.de>
2007-12-29 13:13:23 +0000

    * include/#?expat#?: added includes for expat.library.

---
+++ src/include/clib/expat_protos.h
@@ -0,0 +1,15 @@
+/*
+**      $VER: expat_protos.h 1.0 (1/8/01)
+**
+**      prototypes for expat.library
+**
+*/
+
+#ifndef CLIB_EXPAT_PROTOS_H
+#define CLIB_EXPAT_PROTOS_H
+
+#ifndef LIBRARIES_EXPAT_H
+#include <libraries/expat.h>
+#endif /* LIBRARIES_EXPAT_H */
+
+#endif /* CLIB_EXPAT_PROTOS_H */
