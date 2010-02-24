d0b14cdf64c2ec009b3c4665ea5d545a1da23485
Thore Böckelmann <tboeckel@gmx.de>
2010-02-24 07:54:28 +0000

    * YAM.c: fixed typo.

--- src/YAM.c
+++ src/YAM.c
@@ -2046,7 +2046,7 @@ static void DoStartup(BOOL nocheck, BOOL hide)
   {
   	if(strcmp(lastUserName, currentUserName) != 0)
   	{
-  	  D(DBF_STARTUP, "user '%s' is differs from '%s' of the last start, performing startup actions", currentUserName, lastUserName);
+  	  D(DBF_STARTUP, "user '%s' differs from '%s' of the last start, performing startup actions", currentUserName, lastUserName);
 
       // if the user wishs to delete all old mail during startup of YAM,
       // we do it now
