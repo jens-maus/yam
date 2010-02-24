ba45dbd40310c44e40365d538c9ae4a5c39ce8a4
Thore Böckelmann <tboeckel@gmx.de>
2010-02-24 12:44:54 +0000

    * YAM_CO.c, YAM_FI.c: comparing an empty list against a non-empty list falsely   signaled both lists to be equal. This effectively prevented saving the   conifiguration when i.e. starting with an empty MIME type list and adding   new entries without changing anything else in the configuration.

--- src/YAM_FI.c
+++ src/YAM_FI.c
@@ -1961,40 +1961,50 @@ void FreeFilterRuleList(struct FilterNode *filter)
 static BOOL CompareRuleLists(const struct MinList *rl1, const struct MinList *rl2)
 {
   BOOL equal = TRUE;
-  struct Node *mln1;
-  struct Node *mln2;
+  BOOL empty1;
+  BOOL empty2;
 
   ENTER();
 
-  mln1 = GetHead((struct List *)rl1);
-  mln2 = GetHead((struct List *)rl2);
-
-  while(mln1 != NULL && mln2 != NULL)
+  empty1 = IsMinListEmpty(rl1);
+  empty2 = IsMinListEmpty(rl2);
+  if(empty1 == FALSE && empty2 == FALSE)
   {
-    struct RuleNode *rn1 = (struct RuleNode *)mln1;
-    struct RuleNode *rn2 = (struct RuleNode *)mln2;
+    struct Node *mln1 = GetHead((struct List *)rl1);
+    struct Node *mln2 = GetHead((struct List *)rl2);
 
-    if(rn1->combine           != rn2->combine ||
-       rn1->searchMode        != rn2->searchMode ||
-       rn1->subSearchMode     != rn2->subSearchMode ||
-       rn1->comparison        != rn2->comparison ||
-       rn1->caseSensitive     != rn2->caseSensitive ||
-       rn1->subString         != rn2->subString ||
-       strcmp(rn1->matchPattern, rn2->matchPattern) != 0 ||
-       strcmp(rn1->customField,  rn2->customField) != 0)
+    while(mln1 != NULL && mln2 != NULL)
     {
-      // something of this rule does not match
-      equal = FALSE;
-      break;
+      struct RuleNode *rn1 = (struct RuleNode *)mln1;
+      struct RuleNode *rn2 = (struct RuleNode *)mln2;
+
+      if(rn1->combine           != rn2->combine ||
+         rn1->searchMode        != rn2->searchMode ||
+         rn1->subSearchMode     != rn2->subSearchMode ||
+         rn1->comparison        != rn2->comparison ||
+         rn1->caseSensitive     != rn2->caseSensitive ||
+         rn1->subString         != rn2->subString ||
+         strcmp(rn1->matchPattern, rn2->matchPattern) != 0 ||
+         strcmp(rn1->customField,  rn2->customField) != 0)
+      {
+        // something of this rule does not match
+        equal = FALSE;
+        break;
+      }
+
+      mln1 = GetSucc(mln1);
+      mln2 = GetSucc(mln2);
     }
 
-    mln1 = GetSucc(mln1);
-    mln2 = GetSucc(mln2);
+    // if there are any nodes left then the two lists cannot be equal
+    if((mln1 != NULL && GetSucc(mln1) != NULL) || (mln2 != NULL && GetSucc(mln2) != NULL))
+    {
+      equal = FALSE;
+    }
   }
-
-  // if there are any nodes left then the two lists cannot be equal
-  if((mln1 != NULL && GetSucc(mln1) != NULL) || (mln2 != NULL && GetSucc(mln2) != NULL))
+  else if((empty1 == TRUE && empty2 == FALSE) || (empty1 == FALSE && empty2 == TRUE))
   {
+    // if one list is empty while the other is not the two lists cannot be equal
     equal = FALSE;
   }
 
@@ -2009,47 +2019,57 @@ static BOOL CompareRuleLists(const struct MinList *rl1, const struct MinList *rl
 BOOL CompareFilterLists(const struct MinList *fl1, const struct MinList *fl2)
 {
   BOOL equal = TRUE;
-  struct Node *mln1;
-  struct Node *mln2;
+  BOOL empty1;
+  BOOL empty2;
 
   ENTER();
 
-  mln1 = GetHead((struct List *)fl1);
-  mln2 = GetHead((struct List *)fl2);
-
-  // walk through both lists in parallel and compare the single nodes
-  while(mln1 != NULL && mln2 != NULL)
+  empty1 = IsMinListEmpty(fl1);
+  empty2 = IsMinListEmpty(fl2);
+  if(empty1 == FALSE && empty2 == FALSE)
   {
-    struct FilterNode *fn1 = (struct FilterNode *)mln1;
-    struct FilterNode *fn2 = (struct FilterNode *)mln2;
+    struct Node *mln1 = GetHead((struct List *)fl1);
+    struct Node *mln2 = GetHead((struct List *)fl2);
 
-    // compare every single member of the structure
-    if(fn1->actions         != fn2->actions ||
-       fn1->remote          != fn2->remote ||
-       fn1->applyToNew      != fn2->applyToNew ||
-       fn1->applyOnReq      != fn2->applyOnReq ||
-       fn1->applyToSent     != fn2->applyToSent ||
-       strcmp(fn1->name,       fn2->name) != 0 ||
-       strcmp(fn1->bounceTo,   fn2->bounceTo) != 0 ||
-       strcmp(fn1->forwardTo,  fn2->forwardTo) != 0 ||
-       strcmp(fn1->replyFile,  fn2->replyFile) != 0 ||
-       strcmp(fn1->executeCmd, fn2->executeCmd) != 0 ||
-       strcmp(fn1->playSound,  fn2->playSound) != 0 ||
-       strcmp(fn1->moveTo,     fn2->moveTo) != 0 ||
-       CompareRuleLists(&fn1->ruleList, &fn2->ruleList) == FALSE)
+    // walk through both lists in parallel and compare the single nodes
+    while(mln1 != NULL && mln2 != NULL)
     {
-      // something does not match
-      equal = FALSE;
-      break;
+      struct FilterNode *fn1 = (struct FilterNode *)mln1;
+      struct FilterNode *fn2 = (struct FilterNode *)mln2;
+
+      // compare every single member of the structure
+      if(fn1->actions         != fn2->actions ||
+         fn1->remote          != fn2->remote ||
+         fn1->applyToNew      != fn2->applyToNew ||
+         fn1->applyOnReq      != fn2->applyOnReq ||
+         fn1->applyToSent     != fn2->applyToSent ||
+         strcmp(fn1->name,       fn2->name) != 0 ||
+         strcmp(fn1->bounceTo,   fn2->bounceTo) != 0 ||
+         strcmp(fn1->forwardTo,  fn2->forwardTo) != 0 ||
+         strcmp(fn1->replyFile,  fn2->replyFile) != 0 ||
+         strcmp(fn1->executeCmd, fn2->executeCmd) != 0 ||
+         strcmp(fn1->playSound,  fn2->playSound) != 0 ||
+         strcmp(fn1->moveTo,     fn2->moveTo) != 0 ||
+         CompareRuleLists(&fn1->ruleList, &fn2->ruleList) == FALSE)
+      {
+        // something does not match
+        equal = FALSE;
+        break;
+      }
+
+      mln1 = GetSucc(mln1);
+      mln2 = GetSucc(mln2);
     }
 
-    mln1 = GetSucc(mln1);
-    mln2 = GetSucc(mln2);
+    // if there are any nodes left then the two lists cannot be equal
+    if((mln1 != NULL && GetSucc(mln1) != NULL) || (mln2 != NULL && GetSucc(mln2) != NULL))
+    {
+      equal = FALSE;
+    }
   }
-
-  // if there are any nodes left then the two lists cannot be equal
-  if((mln1 != NULL && GetSucc(mln1) != NULL) || (mln2 != NULL && GetSucc(mln2) != NULL))
+  else if((empty1 == TRUE && empty2 == FALSE) || (empty1 == FALSE && empty2 == TRUE))
   {
+    // if one list is empty while the other is not the two lists cannot be equal
     equal = FALSE;
   }
 
