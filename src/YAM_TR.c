f7bd674e413a1c68a72d98c0941e8cfe466c94f7
Thore Böckelmann <tboeckel@gmx.de>
2010-07-28 12:54:53 +0000

    * YAM_TR.c: avoid empty interface names to be checked.

--- src/YAM_TR.c
+++ src/YAM_TR.c
@@ -1270,13 +1270,16 @@ static BOOL CheckAllInterfaces(const enum TCPIPStack tcpipStack, const struct Li
       if((next = strpbrk(iface, ", ")) != NULL)
         *next++ = '\0';
 
-      // check every single interface to be online and
-      // bail out as soon as we found an active interface
-      D(DBF_NET, "checking interface '%s'", iface);
-      if(CheckSingleInterface(iface, tcpipStack, stackBase) == TRUE)
+      if(iface[0] != '\0')
       {
-        anyIsOnline = TRUE;
-        break;
+        // check every single interface to be online and
+        // bail out as soon as we found an active interface
+        D(DBF_NET, "checking interface '%s'", iface);
+        if(CheckSingleInterface(iface, tcpipStack, stackBase) == TRUE)
+        {
+          anyIsOnline = TRUE;
+          break;
+        }
       }
 
       iface = next;
