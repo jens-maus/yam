aa4d71c8cfa5dbda2520ba9395a25143aba5a8ae
Thore Böckelmann <tboeckel@gmx.de>
2010-08-11 07:28:48 +0000

    * extrasrc/vasprintf.c: make sure that our vasprintf() implementation returns   a NULL buffer in case of an error.

--- src/extrasrc/vasprintf.c
+++ src/extrasrc/vasprintf.c
@@ -44,12 +44,17 @@ int vasprintf(char **ptr, const char * format, va_list ap)
   int ret;
   va_list ap2;
 
+  *ptr = NULL;
+
   VA_COPY(ap2, ap);
   ret = vsnprintf(NULL, 0, format, ap2);
-  if(ret <= 0) return ret;
+  if(ret <= 0)
+    return ret;
 
-  (*ptr) = (char *)malloc(ret+1);
-  if(!*ptr) return -1;
+  *ptr = (char *)malloc(ret+1);
+  if(*ptr == NULL)
+    return -1;
+
   VA_COPY(ap2, ap);
   ret = vsnprintf(*ptr, ret+1, format, ap2);
 
