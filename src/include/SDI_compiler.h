45b8e4bbdc5b02fa11439f0ceac7224afc4f13c8
Thore Böckelmann <tboeckel@gmx.de>
2010-08-17 09:56:57 +0000

    * SDI_compiler.h: updated to latest version.

--- src/include/SDI_compiler.h
+++ src/include/SDI_compiler.h
@@ -4,7 +4,7 @@
 /* Includeheader
 
         Name:           SDI_compiler.h
-        Versionstring:  $VER: SDI_compiler.h 1.34 (26.07.2010)
+        Versionstring:  $VER: SDI_compiler.h 1.33 (03.06.2010)
         Author:         Dirk Stoecker & Jens Langner
         Distribution:   PD
         Project page:   http://www.sf.net/projects/sditools/
@@ -56,9 +56,8 @@
  1.32  28.05.09 : added STACKED definition for non-AROS targets.
  1.33  03.06.10 : added missing SIPTR definition to make SDI_compiler.h more compatible
                   to AROS.
- 1.34  26.07.10 : adapted IPTR and SIPTR definitions as the latest MorphOS SDK already
-                  contains them. (tboeckel)
-
+ 1.34  17.08.10 : added RODATA definition to force the m68k gcc to put constant strings
+                  into the .data section instead of the .text section.
 */
 
 /*
@@ -141,6 +140,8 @@
   #if defined(_M68000) || defined(__M68000) || defined(__mc68000)
     #define REG(reg,arg) arg __asm(#reg)
     #define LREG(reg,arg) register REG(reg,arg)
+    // the m68k gcc places "const char" variables in the .text section
+    #define RODATA __attribute__((section(".data")))
   #else
     #define REG(reg,arg) arg
     #define SAVEDS
@@ -225,10 +226,10 @@
 #if !defined(DEPRECATED)
   #define DEPRECATED
 #endif
-#if !defined(__AROS__) && !defined(__MORPHOS__) && !defined(IPTR)
+#if !defined(__AROS__) && !defined(IPTR)
   #define IPTR ULONG
 #endif
-#if !defined(__AROS__) && !defined(__MORPHOS__) && !defined(SIPTR)
+#if !defined(__AROS__) && !defined(SIPTR)
   #define SIPTR LONG
 #endif
 #if !defined(__AROS__) && !defined(STACKED)
