739f0d68d6e0cf8b353f6a3aaca5375c17f7ab29
Thore Böckelmann <tboeckel@gmx.de>
2010-09-08 19:51:12 +0000

    * mui/TransferControlGroup.c: fixed a NULL pointer access in case the transfer   happens while the transfer window is not visible.

--- src/mui/TransferControlGroup.c
+++ src/mui/TransferControlGroup.c
@@ -79,6 +79,99 @@ struct Data
 /* Hooks */
 
 /* Private Functions */
+/// DoUpdateState
+// update the statistics
+static void DoUpdateStats(struct Data *data, const int size_incr, const char *status)
+{
+  ENTER();
+
+  if(size_incr > 0)
+  {
+    data->Size_Done += size_incr;
+    data->Size_Curr += size_incr;
+  }
+  else if(size_incr == TCG_SETMAX)
+  {
+    // first update the total transferred size
+    data->Size_Done += data->Size_Curr_Max - data->Size_Curr;
+    // we are done with this mail, so make sure the current size equals the final size
+    data->Size_Curr = data->Size_Curr_Max;
+  }
+
+  // update the stats 4 times per second at most
+  if(TimeHasElapsed(&data->Clock_Last, 250000) == TRUE)
+  {
+    ULONG deltatime = data->Clock_Last.Seconds - data->Clock_Start;
+    ULONG speed = 0;
+    LONG remclock = 0;
+    ULONG max;
+    ULONG current;
+
+    // if we have a preselection list then update this as well
+    if(data->preselectionList != NULL && data->Msgs_ListPos >= 0)
+      set(data->preselectionList, MUIA_NList_Active, data->Msgs_ListPos);
+
+    // first we calculate the speed in bytes/sec
+    // to display to the user
+    if(deltatime != 0)
+      speed = data->Size_Done / deltatime;
+
+    // calculate the estimated remaining time
+    if(speed != 0 && ((remclock = (data->Size_Tot / speed) - deltatime) < 0))
+      remclock = 0;
+
+    // show the current status
+    set(data->TX_STATUS, MUIA_Text_Contents, status);
+
+    // show the current message index
+    set(data->GA_COUNT, MUIA_Gauge_Current, data->Msgs_Curr);
+
+    // format the size done and size total strings
+    FormatSize(data->Size_Done, data->str_size_done, sizeof(data->str_size_done), SF_MIXED);
+    FormatSize(data->Size_Tot, data->str_size_tot, sizeof(data->str_size_tot), SF_MIXED);
+    FormatSize(data->Size_Curr, data->str_size_curr, sizeof(data->str_size_curr), SF_AUTO);
+
+    FormatSize(speed, data->str_speed, sizeof(data->str_speed), SF_MIXED);
+
+    // now format the StatsLabel and update it
+    snprintf(data->stats_label, sizeof(data->stats_label), tr(MSG_TR_TRANSFERSTATUS),
+                                data->str_size_done, data->str_size_tot, data->str_speed,
+                                deltatime / 60, deltatime % 60,
+                                remclock / 60, remclock % 60);
+
+    set(data->TX_STATS, MUIA_Text_Contents, data->stats_label);
+
+    // update the gauge
+    snprintf(data->size_gauge_label, sizeof(data->size_gauge_label), tr(MSG_TR_TRANSFERSIZE),
+                                                           data->str_size_curr, data->str_size_curr_max);
+
+    if(size_incr == TCG_SETMAX)
+    {
+      max = 100;
+      current = 100;
+    }
+    else if(data->Size_Curr_Max <= 65536)
+    {
+      max = data->Size_Curr_Max;
+      current = data->Size_Curr;
+    }
+    else
+    {
+      max = data->Size_Curr_Max / 1024;
+      current = data->Size_Curr / 1024;
+    }
+    xset(data->GA_BYTES, MUIA_Gauge_InfoText, data->size_gauge_label,
+                         MUIA_Gauge_Max,      max,
+                         MUIA_Gauge_Current,  current);
+
+    // signal the application to update now
+    DoMethod(G->App, MUIM_Application_InputBuffered);
+  }
+
+  LEAVE();
+}
+
+///
 
 /* Overloaded Methods */
 /// OVERLOAD(OM_NEW)
@@ -311,7 +404,7 @@ DECLARE(Next) // int index, int listpos, ULONG size, const char *status
   // format the current mail's size ahead of any refresh
   FormatSize(msg->size, data->str_size_curr_max, sizeof(data->str_size_curr_max), SF_AUTO);
 
-  DoMethod(obj, METHOD(Update), 0, msg->status);
+  DoUpdateStats(data, 0, msg->status);
 
   LEAVE();
   return 0;
@@ -327,92 +420,7 @@ DECLARE(Update) // int size_incr, const char *status
 
   ENTER();
 
-  if(msg->size_incr > 0)
-  {
-    data->Size_Done += msg->size_incr;
-    data->Size_Curr += msg->size_incr;
-  }
-  else if(msg->size_incr == TCG_SETMAX)
-  {
-    // first update the total transferred size
-    data->Size_Done += data->Size_Curr_Max - data->Size_Curr;
-    // we are done with this mail, so make sure the current size equals the final size
-    data->Size_Curr = data->Size_Curr_Max;
-  }
-
-  // if the window isn't open we don't need to update it, do we?
-  if(xget(_win(obj), MUIA_Window_Open) == TRUE)
-  {
-    // update the stats 4 times per second at most
-    if(TimeHasElapsed(&data->Clock_Last, 250000) == TRUE)
-    {
-      ULONG deltatime = data->Clock_Last.Seconds - data->Clock_Start;
-      ULONG speed = 0;
-      LONG remclock = 0;
-      ULONG max;
-      ULONG current;
-
-      // if we have a preselection list then update this as well
-      if(data->preselectionList != NULL && data->Msgs_ListPos >= 0)
-        set(data->preselectionList, MUIA_NList_Active, data->Msgs_ListPos);
-
-      // first we calculate the speed in bytes/sec
-      // to display to the user
-      if(deltatime != 0)
-        speed = data->Size_Done / deltatime;
-
-      // calculate the estimated remaining time
-      if(speed != 0 && ((remclock = (data->Size_Tot / speed) - deltatime) < 0))
-        remclock = 0;
-
-      // show the current status
-      set(data->TX_STATUS, MUIA_Text_Contents, msg->status);
-
-      // show the current message index
-      set(data->GA_COUNT, MUIA_Gauge_Current, data->Msgs_Curr);
-
-      // format the size done and size total strings
-      FormatSize(data->Size_Done, data->str_size_done, sizeof(data->str_size_done), SF_MIXED);
-      FormatSize(data->Size_Tot, data->str_size_tot, sizeof(data->str_size_tot), SF_MIXED);
-      FormatSize(data->Size_Curr, data->str_size_curr, sizeof(data->str_size_curr), SF_AUTO);
-
-      FormatSize(speed, data->str_speed, sizeof(data->str_speed), SF_MIXED);
-
-      // now format the StatsLabel and update it
-      snprintf(data->stats_label, sizeof(data->stats_label), tr(MSG_TR_TRANSFERSTATUS),
-                                  data->str_size_done, data->str_size_tot, data->str_speed,
-                                  deltatime / 60, deltatime % 60,
-                                  remclock / 60, remclock % 60);
-
-      set(data->TX_STATS, MUIA_Text_Contents, data->stats_label);
-
-      // update the gauge
-      snprintf(data->size_gauge_label, sizeof(data->size_gauge_label), tr(MSG_TR_TRANSFERSIZE),
-                                                             data->str_size_curr, data->str_size_curr_max);
-
-      if(msg->size_incr == TCG_SETMAX)
-      {
-        max = 100;
-        current = 100;
-      }
-      else if(data->Size_Curr_Max <= 65536)
-      {
-        max = data->Size_Curr_Max;
-        current = data->Size_Curr;
-      }
-      else
-      {
-        max = data->Size_Curr_Max / 1024;
-        current = data->Size_Curr / 1024;
-      }
-      xset(data->GA_BYTES, MUIA_Gauge_InfoText, data->size_gauge_label,
-                           MUIA_Gauge_Max,      max,
-                           MUIA_Gauge_Current,  current);
-
-      // signal the application to update now
-      DoMethod(G->App, MUIM_Application_InputBuffered);
-    }
-  }
+  DoUpdateStats(data, msg->size_incr, msg->status);
 
   LEAVE();
   return 0;
