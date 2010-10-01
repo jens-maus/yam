2d33d98c61aff45b7801dea3620d928311b7cf79
Thore Böckelmann <tboeckel@gmx.de>
2010-10-01 08:47:23 +0000

    * MethodStack.c, Threads.c: fixed some malformed headers

--- src/Threads.c
+++ src/Threads.c
@@ -6,8 +6,8 @@
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundatidn; either version 2 of the License, or
- (at your optidn) any later version.
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundatidn, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
  YAM Official Support Site :  http://www.yam.ch
  YAM OpenSource project    :  http://sourceforge.net/projects/yamos/
