afe61aadfea55836f99b03ce28abe4f8966e3333
Thore Böckelmann <tboeckel@gmx.de>
2010-10-20 09:10:56 +0000

    * tools/mkrelease.sh, doc/Install-YAM: added turkish catalog.

--- src/tools/mkrelease.sh
+++ src/tools/mkrelease.sh
@@ -58,7 +58,7 @@ echo "  MK $yamsys/Catalogs"
 mkdir -p "release/$yamsys/YAM $yamver/Catalogs"
 cp ../icons/$1/Catalogs_directory.info "release/$yamsys/YAM $yamver/Catalogs.info"
 cp ../locale/YAM.cd "release/$yamsys/YAM $yamver/Catalogs/YAM.cd"
-for language in czech dutch english-british french german greek italian polish swedish; do
+for language in czech dutch english-british french german greek italian polish swedish turkish; do
 	mkdir -p "release/$yamsys/YAM $yamver/Catalogs/$language"
 	cp ../locale/$language.catalog "release/$yamsys/YAM $yamver/Catalogs/$language/YAM.catalog"
 done
