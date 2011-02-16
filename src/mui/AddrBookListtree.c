1c4d54a068651ed1695b13c75092d8f3a036a86f
Thore Böckelmann <tboeckel@gmx.de>
2011-02-16 15:19:01 +0000

    * YAM_AB.c, mui/AddrBookListtree.c: converted the standard list hooks to   proper methods.

--- src/mui/AddrBookListtree.c
+++ src/mui/AddrBookListtree.c
@@ -35,9 +35,12 @@
 #include <mui/NListtree_mcc.h>
 
 #include "YAM.h"
+#include "YAM_addressbook.h"
 #include "YAM_addressbookEntry.h"
+#include "YAM_config.h"
 
 #include "AVLTree.h"
+#include "Locale.h"
 #include "MailList.h"
 #include "MUIObjects.h"
 
@@ -51,6 +54,8 @@ struct Data
 {
   Object *listImage;
   struct AVL_Tree *avlTree;        // the address book as an AVL tree
+  char dateStr[SIZE_SMALL];
+  char aliasStr[SIZE_DEFAULT];
 };
 */
 
@@ -78,6 +83,14 @@ OVERLOAD(OM_NEW)
   if((avlTree = CreateAVLTree(ComparePersons)) != NULL)
   {
     if((obj = DoSuperNew(cl, obj,
+
+      InputListFrame,
+      MUIA_NListtree_DragDropSort,     TRUE,
+      MUIA_NListtree_Title,            TRUE,
+      MUIA_NListtree_EmptyNodes,       TRUE,
+      MUIA_NList_DefaultObjectOnClick, FALSE,
+      MUIA_Font,                       C->FixedFontList ? MUIV_NList_Font_Fixed : MUIV_NList_Font,
+
       TAG_MORE, inittags(msg))) != NULL)
     {
       GETDATA;
@@ -116,6 +129,159 @@ OVERLOAD(OM_DISPOSE)
   data->avlTree = NULL;
 
   return DoSuperMethodA(cl, obj, msg);
+}
+
+///
+/// OVERLOAD(MUIM_NListtree_Construct)
+OVERLOAD(MUIM_NListtree_Construct)
+{
+  struct MUIP_NListtree_Construct *ncm = (struct MUIP_NListtree_Construct *)msg;
+  struct ABEntry *addr = (struct ABEntry *)ncm->UserData;
+  struct ABEntry *entry;
+
+  ENTER();
+
+  if((entry = memdup(addr, sizeof(*addr))) != NULL)
+  {
+    // clone the member list aswell
+    if(addr->Members != NULL)
+    {
+      if((entry->Members = strdup(addr->Members)) == NULL)
+      {
+        // if strdup() failed then we let the whole function fail
+        free(entry);
+        entry = NULL;
+      }
+    }
+  }
+
+  RETURN(entry);
+  return (IPTR)entry;
+}
+
+///
+/// OVERLOAD(MUIM_NListtree_Destruct)
+OVERLOAD(MUIM_NListtree_Destruct)
+{
+  struct MUIP_NListtree_Destruct *ndm = (struct MUIP_NListtree_Destruct *)msg;
+  struct ABEntry *entry = (struct ABEntry *)ndm->UserData;
+
+  ENTER();
+
+  if(entry != NULL)
+  {
+    free(entry->Members);
+    free(entry);
+  }
+
+  LEAVE();
+  return 0;
+}
+
+///
+/// OVERLOAD(MUIM_NListtree_Compare)
+OVERLOAD(MUIM_NListtree_Compare)
+{
+  struct MUIP_NListtree_Compare *ncm = (struct MUIP_NListtree_Compare *)msg;
+  struct ABEntry *ab1 = (struct ABEntry *)ncm->TreeNode1->tn_User;
+  struct ABEntry *ab2 = (struct ABEntry *)ncm->TreeNode2->tn_User;
+  char *n1, *n2;
+  int cmp = 0;
+
+  switch(G->AB->SortBy)
+  {
+    case 1:
+      if((n1 = strrchr(ab1->RealName,' ')) == NULL)
+        n1 = ab1->RealName;
+      if((n2 = strrchr(ab2->RealName,' ')) == NULL)
+        n2 = ab2->RealName;
+      cmp = Stricmp(n1, n2);
+    break;
+
+    case 2:
+      cmp = Stricmp(ab1->RealName, ab2->RealName);
+    break;
+
+    case 3:
+      cmp = Stricmp(ab1->Comment, ab2->Comment);
+    break;
+
+    case 4:
+      cmp = Stricmp(ab1->Address, ab2->Address);
+    break;
+  }
+
+  if(cmp == 0)
+    cmp = Stricmp(ab1->Alias, ab2->Alias);
+
+  return cmp;
+}
+
+///
+/// OVERLOAD(MUIM_NListtree_Display)
+OVERLOAD(MUIM_NListtree_Display)
+{
+  struct MUIP_NListtree_Display *ndm = (struct MUIP_NListtree_Display *)msg;
+
+  ENTER();
+
+  if(ndm->TreeNode != NULL)
+  {
+    GETDATA;
+    struct ABEntry *entry = (struct ABEntry *)ndm->TreeNode->tn_User;
+
+    AB_ExpandBD(entry->BirthDay, data->dateStr, sizeof(data->dateStr));
+
+    ndm->Array[0]  = entry->Alias;
+    ndm->Array[1]  = entry->RealName;
+    ndm->Array[2]  = entry->Comment;
+    ndm->Array[3]  = entry->Address;
+    ndm->Array[4]  = entry->Street;
+    ndm->Array[5]  = entry->City;
+    ndm->Array[6]  = entry->Country;
+    ndm->Array[7]  = entry->Phone;
+    ndm->Array[8]  = data->dateStr;
+    ndm->Array[9]  = entry->PGPId;
+    ndm->Array[10] = entry->Homepage;
+
+    switch(entry->Type)
+    {
+       case AET_LIST:
+       {
+         snprintf(data->aliasStr, sizeof(data->aliasStr), "\033o[0]%s", entry->Alias);
+         ndm->Array[0] = data->aliasStr;
+       }
+       break;
+
+       case AET_GROUP:
+       {
+         ndm->Preparse[0] = (char *)MUIX_B;
+         ndm->Preparse[2] = (char *)MUIX_B;
+       }
+       break;
+
+       default:
+         // nothing
+       break;
+    }
+  }
+  else
+  {
+    ndm->Array[0]  = (STRPTR)tr(MSG_AB_TitleAlias);
+    ndm->Array[1]  = (STRPTR)tr(MSG_AB_TitleName);
+    ndm->Array[2]  = (STRPTR)tr(MSG_AB_TitleDescription);
+    ndm->Array[3]  = (STRPTR)tr(MSG_AB_TitleAddress);
+    ndm->Array[4]  = (STRPTR)tr(MSG_AB_TitleStreet);
+    ndm->Array[5]  = (STRPTR)tr(MSG_AB_TitleCity);
+    ndm->Array[6]  = (STRPTR)tr(MSG_AB_TitleCountry);
+    ndm->Array[7]  = (STRPTR)tr(MSG_AB_TitlePhone);
+    ndm->Array[8]  = (STRPTR)tr(MSG_AB_TitleBirthDate);
+    ndm->Array[9]  = (STRPTR)tr(MSG_AB_TitlePGPId);
+    ndm->Array[10] = (STRPTR)tr(MSG_AB_TitleHomepage);
+  }
+
+  LEAVE();
+  return 0;
 }
 
 ///
