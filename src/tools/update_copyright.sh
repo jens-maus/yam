d38e4946d9e9f14bc9119c8ae566dc6cd16225d0
Jens Maus <mail@jens-maus.de>
2012-01-02 11:10:52 +0000

    - added update_copyright.sh shell script for future copyright notices bumps.

---
+++ src/tools/update_copyright.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# simply bumps the year in all copyright and header files to the latest one
+#
+# WARNING: This script will run over ALL files regardless if they are text
+# or binary files and search for the copyright notice and replace it
+# immediately. So you need to be aware of that
+#
+
+YEAR=`date +%Y`
+
+# walk through the whole directory this script is called in and search
+# for files which we will try to update the Copyright notice
+find . \( -not -path "*/.svn/*" -not -name "update_copyright.sh" \) -type f \
+       -exec sed -i "s/Copyright.*(C).*2000-.*YAM Open Source Team/Copyright (C) 2000-${YEAR} YAM Open Source Team/g" {} \;
+
