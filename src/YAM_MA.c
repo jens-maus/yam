8cfaf589cf7b65dd23e67894e40fc17b56b88bb1
Thore Böckelmann <tboeckel@gmx.de>
2013-08-30 11:13:53 +0000

    * YAM_MA.c: don't the the "Forward" menu item's short cut in the main window's constructor as this is done later another time anyway while setting up all dynamic menus.

--- src/YAM_MA.c
+++ src/YAM_MA.c
@@ -4399,12 +4399,6 @@ struct MA_ClassData *MA_New(void)
       DoMethod(data->GUI.TO_TOOLBAR, MUIM_TheBar_Notify, TB_MAIN_FIND,     MUIA_Pressed, FALSE, MUIV_Notify_Application, 2, MUIM_YAMApplication_OpenSearchMailWindow, NULL);
       DoMethod(data->GUI.TO_TOOLBAR, MUIM_TheBar_Notify, TB_MAIN_ADDRBOOK, MUIA_Pressed, FALSE, MUIV_Notify_Application, 3, MUIM_CallHook, &AB_OpenHook, ABM_EDIT);
       DoMethod(data->GUI.TO_TOOLBAR, MUIM_TheBar_Notify, TB_MAIN_CONFIG,   MUIA_Pressed, FALSE, MUIV_Notify_Application, 1, MUIM_YAMApplication_OpenConfigWindow);
-
-      // define shortcut for "Forward" item
-      if(C->ForwardMode == FWM_ATTACH)
-        set(data->GUI.MI_FORWARD_ATTACH, MUIA_Menuitem_Shortcut, "W");
-      else if(C->ForwardMode == FWM_INLINE)
-        set(data->GUI.MI_FORWARD_INLINE, MUIA_Menuitem_Shortcut, "W");
     }
   }
   else
