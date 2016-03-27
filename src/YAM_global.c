c0ef0f7c070fa65804eccb524a4896f129b77a1f
Jens Maus <mail@jens-maus.de>
2016-03-27 23:03:46 +0000

    - bumped copyright year to 2016.

--- src/YAM_global.c
+++ src/YAM_global.c
@@ -2,7 +2,7 @@
 
  YAM - Yet Another Mailer
  Copyright (C) 1995-2000 Marcel Beck
- Copyright (C) 2000-2015 YAM Open Source Team
+ Copyright (C) 2000-2016 YAM Open Source Team
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -117,7 +117,7 @@ static const char USED_VAR yam_stack_size[] = "$STACK:" STR(SIZE_STACK) "\n";
 #ifndef __YAM_BUILDID
 #define __YAM_BUILDID   0
 #endif
-#define __YAM_COPYRIGHT     "Copyright (C) 2000-2015 YAM Open Source Team"
+#define __YAM_COPYRIGHT     "Copyright (C) 2000-2016 YAM Open Source Team"
 #define __YAM_FULLCOPYRIGHT "Copyright (C) 1995-2000 Marcel Beck\n" __YAM_COPYRIGHT
 
 // find out something about the compiler used
