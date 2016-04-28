1f018e7479b12a1dd5e98846c0520a18647a4520
Jens Maus <mail@jens-maus.de>
2016-04-28 16:28:48 +0200

    updated the amissl4 header files according to latest changes in AmiSSL project.

--- src/include/pragmas/amisslmaster_pragmas.h
+++ src/include/pragmas/amisslmaster_pragmas.h
@@ -1,14 +1,14 @@
-/* Automatically generated header (sfdc 1.8)! Do not edit! */
+/* Automatically generated header (sfdc 1.10)! Do not edit! */
 #ifndef PRAGMAS_AMISSLMASTER_PRAGMAS_H
 #define PRAGMAS_AMISSLMASTER_PRAGMAS_H
 
 /*
-**	$VER: amisslmaster_pragmas.h $Id$ $Id$
+**   $VER: amisslmaster_pragmas.h $Id$ $Id$
 **
-**	Direct ROM interface (pragma) definitions.
+**   Direct ROM interface (pragma) definitions.
 **
-**	Copyright © 2001 Amiga, Inc.
-**	    All Rights Reserved
+**   Copyright © 2001 Amiga, Inc.
+**       All Rights Reserved
 */
 
 #if defined(LATTICE) || defined(__SASC) || defined(_DCC)
