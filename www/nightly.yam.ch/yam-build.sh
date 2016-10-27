e26efa93aa21b03253533e914af4e36b698de66d
Jens Maus <mail@jens-maus.de>
2016-10-27 10:47:55 +0200

    we use "git clean -d -x -f" to cleanup the local git working copy before doing a fresh pull.

--- www/nightly.yam.ch/yam-build.sh
+++ www/nightly.yam.ch/yam-build.sh
@@ -249,7 +249,7 @@ fi
 
 # undo all possible local changes to the local working copy so that
 # we won't get any conflicts.
-$(cd ${MODULE}; ${GIT} checkout . 2>&1 >/dev/null)
+$(cd ${MODULE}; ${GIT} clean -d -x -f 2>&1 >/dev/null)
 
 # now pull changes
 output=`cd ${MODULE}; ${GIT} pull 2>&1 | egrep "^Updating .{7}\.\..{7}" | cut -d' ' -f2`
