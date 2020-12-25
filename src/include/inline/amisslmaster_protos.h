67f0cf5f82547b67c57e42c630189a2079cee840
Pär Boberg <pbobbenb@icloud.com>
2020-12-25 13:47:51 +0100

    Bumped AmiSSL includes to the latest available (4.7)

--- src/include/inline/amisslmaster_protos.h
+++ src/include/inline/amisslmaster_protos.h
@@ -1,23 +1,30 @@
-#ifndef _VBCCINLINE_AMISSLMASTER_H
-#define _VBCCINLINE_AMISSLMASTER_H
+/* Automatically generated header (sfdc 1.11)! Do not edit! */
 
-#ifndef EXEC_TYPES_H
-#include <exec/types.h>
-#endif
+#ifndef _INLINE_AMISSLMASTER_H
+#define _INLINE_AMISSLMASTER_H
 
-LONG __InitAmiSSLMaster(__reg("a6") struct Library *, __reg("d0") LONG APIVersion, __reg("d1") LONG AllowUserStructs)="\tjsr\t-30(a6)";
-#define InitAmiSSLMaster(APIVersion, AllowUserStructs) __InitAmiSSLMaster(AmiSSLMasterBase, (APIVersion), (AllowUserStructs))
+#ifndef _SFDC_VARARG_DEFINED
+#define _SFDC_VARARG_DEFINED
+#ifdef __HAVE_IPTR_ATTR__
+typedef APTR _sfdc_vararg __attribute__((iptr));
+#else
+typedef ULONG _sfdc_vararg;
+#endif /* __HAVE_IPTR_ATTR__ */
+#endif /* _SFDC_VARARG_DEFINED */
 
-struct Library * __OpenAmiSSL(__reg("a6") struct Library *)="\tjsr\t-36(a6)";
+LONG __InitAmiSSLMaster(__reg("a6") struct Library * , __reg("d0") LONG APIVersion , __reg("d1") LONG UsesOpenSSLStructs ) = "\tjsr\t-30(a6)";
+#define InitAmiSSLMaster(APIVersion, UsesOpenSSLStructs) __InitAmiSSLMaster(AmiSSLMasterBase, (APIVersion), (UsesOpenSSLStructs))
+
+struct Library * __OpenAmiSSL(__reg("a6") struct Library * ) = "\tjsr\t-36(a6)";
 #define OpenAmiSSL() __OpenAmiSSL(AmiSSLMasterBase)
 
-void __CloseAmiSSL(__reg("a6") struct Library *)="\tjsr\t-42(a6)";
+void __CloseAmiSSL(__reg("a6") struct Library * ) = "\tjsr\t-42(a6)";
 #define CloseAmiSSL() __CloseAmiSSL(AmiSSLMasterBase)
 
-struct Library * __OpenAmiSSLCipher(__reg("a6") struct Library *, __reg("d0") LONG Cipher)="\tjsr\t-48(a6)";
+struct Library * __OpenAmiSSLCipher(__reg("a6") struct Library * , __reg("d0") LONG Cipher ) = "\tjsr\t-48(a6)";
 #define OpenAmiSSLCipher(Cipher) __OpenAmiSSLCipher(AmiSSLMasterBase, (Cipher))
 
-void __CloseAmiSSLCipher(__reg("a6") struct Library *, __reg("a0") struct Library * CipherBase)="\tjsr\t-54(a6)";
+void __CloseAmiSSLCipher(__reg("a6") struct Library * , __reg("a0") struct Library * CipherBase ) = "\tjsr\t-54(a6)";
 #define CloseAmiSSLCipher(CipherBase) __CloseAmiSSLCipher(AmiSSLMasterBase, (CipherBase))
 
-#endif /*  _VBCCINLINE_AMISSLMASTER_H  */
+#endif /* !_INLINE_AMISSLMASTER_H */
--- src/include/openssl/bio.h
+++ src/include/openssl/bio.h
@@ -2,7 +2,7 @@
 #include <proto/amissl.h>
 #endif
 /*
- * Copyright 1995-2018 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 1995-2020 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -22,10 +22,6 @@
 
 # include <openssl/crypto.h>
 # include <openssl/bioerr.h>
-
-# ifndef OPENSSL_NO_SCTP
-#  include <openssl/e_os2.h>
-# endif
 
 #ifdef  __cplusplus
 extern "C" {
@@ -176,6 +172,7 @@ extern "C" {
  */
 # define BIO_FLAGS_MEM_RDONLY    0x200
 # define BIO_FLAGS_NONCLEAR_RST  0x400
+# define BIO_FLAGS_IN_EOF        0x800
 
 typedef union bio_addr_st BIO_ADDR;
 typedef struct bio_addrinfo_st BIO_ADDRINFO;
